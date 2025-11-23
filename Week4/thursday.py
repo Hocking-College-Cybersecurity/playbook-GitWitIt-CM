b04cddbc235cc316e72bca2dd3a6f9c506621372
GitWitIt-CM <cmzx222@gmail.com>
2025-11-22 20:14:30 -0500

    Function_lab.py

---
+++ Week4/thursday.py
@@ -0,0 +1,13 @@
+my_list = [1, 2, 3]
+
+my_tuple = (4, 5, 6)
+
+my_dictionary = {"a": 1, "b": 2}
+
+my_dictionary["c"] = 3      # add
+my_dictionary["a"] = 10     # change
+del my_dictionary["b"]      # remove
+
+print(my_list)
+print(my_tuple)
+print(my_dictionary)
---
+++ Week5/monday.py
@@ -0,0 +1,26 @@
+import sys 
+import csv
+
+data_filename = "data.csv"
+with open(data_filename, "r") as my_data:
+    data_dict = csv.DictReader(my_data)
+    for row in data_dict:
+        print(f"{row}")
+
+print(data_dict)
+data_dict[0]{"ID":'34'}
+
+
+print(data_dict)
+data_dict[0][IsADirectoryError]
+
+R
+
+
+
+
+
+people = {"Jerry": 15, "Tom": 6, "Bob": 12}
+
+filename = "example.txt"
+
